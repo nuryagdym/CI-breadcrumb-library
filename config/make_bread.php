919ca00d1b54a4ca5d3a040c668732eb545afc4b
nuryagdym <26792980+nuryagdym@users.noreply.github.com>
2020-02-25 12:03:32 +0300

    support for multiple templates and custom anchor format

--- config/make_bread.php
+++ config/make_bread.php
@@ -1,15 +1,30 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-
-// $config['include_home'] will tell the library if the first element should be the homepage. You only put the title of the first crumb. If you leave it blank it will not put homepage as first crumb
-$config['include_home'] = 'First page';
-// $config['divider'] is the divider you want between the crumbs. Leave blank if you don't want a divider;
-$config['divider'] = '&nbsp;&#8250;&nbsp;';
-// $config['container_open'] is the opening tag of the breadcrumb container
-$config['container_open'] = '<div id="breadcrumb">';
-// $config['container_close'] is the closing tag of the breadcrumb container
-$config['container_close'] = '</div>';
-// $config['crumb_open'] is the opening tag of the crumb container
-$config['crumb_open'] = '';
-// $config['crumb_close'] is the closing tag of the crumb container
-$config['crumb_close'] = '';
+//you can define more than one format for breadcrumbs. one for example for you admin dashboard and other for your user's dashboard/website
+$config['make_bread_default'] = array(
+  //sub_url will be added before all urls. for example, if /users path is added and sub_url = admin then breadcrumb URL will be https://example.com/admin/users and so on.
+	'sub_url' => '',
+  
+  //will tell the library if the first element should be the homepage. You only put the title of the first crumb. If you leave it blank it will not put homepage as first crumb
+	'include_home' => '<li class="m-nav__item m-nav__item--home"><a href="#" class="m-nav__link m-nav__link--icon"><i class="m-nav__link-icon la la-home"></i></a></li>',
+  
+  //the divider you want between the crumbs. Leave blank if you don't want a divider;
+	'divider' => '<li class="m-nav__separator">-</li>',
+  
+  //the opening tag of the breadcrumb container
+	'container_open' => '<ol itemscope itemtype="https://schema.org/BreadcrumbList" class="m-subheader__breadcrumbs m-nav m-nav--inline">',
+  
+  //the closing tag of the breadcrumb container
+	'container_close' => '</ol>',
+  
+  //the opening tag of the crumb container
+	'crumb_open' => '<li class="m-nav__item">',
+  
+  //the closing tag of the crumb container
+	'crumb_close' => '</li>',
+  
+  //format of the anchor tags.
+	'anchor_format' => '<a itemprop="item" href="%s" class="m-nav__link">
+                    <span itemprop="name" class="m-nav__link-text">%s</span></a>
+                    <meta itemprop="position" content="%d" />'
+);
